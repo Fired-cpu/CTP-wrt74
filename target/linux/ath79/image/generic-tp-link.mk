9d0a4298a7fd9bc9500cb5289e942c7bf93b3b80
Witch <flame.weiz@gmail.com>
2026-08-19 23:39:05 +0700

    welcome wdr74 to the entry, generic-tp-link.mk
    Signed-off-by: Witch <flame.weiz@gmail.com>

--- target/linux/ath79/image/generic-tp-link.mk
+++ target/linux/ath79/image/generic-tp-link.mk
@@ -666,6 +666,22 @@ define Device/tplink_tl-wdr6500-v2
 endef
 TARGET_DEVICES += tplink_tl-wdr6500-v2
 
+define Device/tplink_tl-wdr7400-v2
+  $(Device/tplink-8mlzma)
+  SOC := qca9561
+  DEVICE_MODEL := TL-WDR7400
+  DEVICE_VARIANT := v2
+  DEVICE_PACKAGES := kmod-usb-core kmod-usb2 kmod-usb-ledtrig-usbport \
+	kmod-ath9k kmod-ath10k-ct ath10k-firmware-qca988x-ct
+  IMAGE_SIZE := 8000k
+  KERNEL := kernel-bin | append-dtb | lzma | uImage lzma
+  KERNEL_INITRAMFS := kernel-bin | append-dtb | lzma | uImage lzma
+  TPLINK_HWID := 0x74000002
+  TPLINK_HEADER_VERSION := 1
+  SUPPORTED_DEVICES += tl-wdr7400-v2
+endef
+TARGET_DEVICES += tplink_tl-wdr7400-v2
+
 define Device/tplink_tl-wdr7500-v3
   $(Device/tplink-8mlzma)
   SOC := qca9558
